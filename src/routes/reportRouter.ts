265a53ce293df95172529d80d898c47e012d2ab2
ismaG12 <113376063+ismaG12@users.noreply.github.com>
2023-11-20 14:39:46 -0600

    reports

--- src/routes/reportRouter.ts
+++ src/routes/reportRouter.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { getAllMovements, getMovementsByBranch, getMovementsByVehicle,getVehicleAllMovements } from '../scripts/reports/movements';
+import { getAllGoals, getAllMovements, getMovementsByBranch, getMovementsByVehicle,getVehicleAllMovements } from '../scripts/reports/movements';
 import * as ExcelJS from 'exceljs';
 
 export const reportRouter = Router();
@@ -75,7 +75,7 @@ reportRouter.get('/vehicle/movements', async (req:Request, res:Response) => {
 
 reportRouter.get('/goals', async (req:Request, res:Response) => {
     try {
-        const data = await getVehicleAllMovements()
+        const data = await getAllGoals()
         const file = await generarExcel(data);
         res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         res.setHeader('Content-Disposition', 'attachment; filename=datos.xlsx');
